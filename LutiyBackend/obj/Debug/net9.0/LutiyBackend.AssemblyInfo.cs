90c0f68b5091b68139ae80c7aaaded6a0f119a1e
foxiinc <kirill20061231@gmail.com>
2024-11-28 11:03:27 +0700

    dopil

--- LutiyBackend/obj/Debug/net9.0/LutiyBackend.AssemblyInfo.cs
+++ LutiyBackend/obj/Debug/net9.0/LutiyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LutiyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1e24b01bd12462c6be6dba445bde23ebec35ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e806a4b69b0f1dcf5904a0a992bf5a0b3653acb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LutiyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LutiyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
